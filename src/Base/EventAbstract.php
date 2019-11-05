340ad881e7da11be606709828d3b954e42763083
dangliuhui <liuhuidang@sf-express.com>
2019-11-05 11:39:49 +0800

    update

--- src/Base/EventAbstract.php
+++ src/Base/EventAbstract.php
@@ -11,11 +11,11 @@ abstract class EventAbstract
     final public function main()
     {
         $stime = microtime(true);
-        Log::alert(sprintf('%s start!', static::class));
+        Log::debug(sprintf('%s start!', static::class));
         $params = func_get_args();
         $this->run($params);
         $etime = microtime(true);
         $time  = number_format(($etime - $stime) * 1000, 3, '.', '');
-        Log::alert(sprintf('%s end! time:' . $time . 'ms', static::class));
+        Log::debug(sprintf('%s end! time:' . $time . 'ms', static::class));
     }
 }
