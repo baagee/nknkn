a9c787d6b3c280194cf3cae3aaa0177aaafd024a
党留辉 <liuhuidang@sf-express.com>
2020-03-15 10:36:25 +0800

    update IdGenerator.php

--- src/IdGenerator.php
+++ src/IdGenerator.php
@@ -22,44 +22,63 @@ final class IdGenerator
     private const KEY_PREFIX = 'COMMON:ID:';
 
     /**
+     * @param bool $useRedis 是否使用redis生成
      * @return int
      * @throws \Exception
      */
-    final public static function getOne()
+    final public static function getOne(bool $useRedis = true)
     {
-        $redis = Redis::getConnection();
-        $key   = self::KEY_PREFIX . Config::get('app/app_name', '');
-        return intval(time() * 10000 + $redis->incr($key) * 10000 + rand(0, 10000));
-    }
-
-    /**
-     * @param int $count
-     * @return \Generator
-     * @throws \Exception
-     */
-    final public static function getYield(int $count)
-    {
-        $redis  = Redis::getConnection();
-        $key    = self::KEY_PREFIX . Config::get('app/app_name', '');
-        $number = $redis->incrby($key, $count);
-        for ($i = 0; $i < $count; $i++) {
-            yield intval(time() * 10000 + ($number - $count + $i + 1) * 10000 + rand(0, 10000));
+        if ($useRedis) {
+            $redis = Redis::getConnection();
+            $key   = self::KEY_PREFIX . Config::get('app/app_name', '');
+            return intval(time() * 10000 + $redis->incr($key) * 10000 + rand(0, 10000));
+        } else {
+            return intval(microtime(true) * 1000000) + mt_rand(1000000, 9999999);
         }
     }
 
     /**
-     * @param int $count
+     * @param int  $count    个数
+     * @param bool $useRedis 是否使用redis生成
+     * @return \Generator
+     * @throws \Exception
+     */
+    final public static function getYield(int $count, bool $useRedis = true)
+    {
+        if ($useRedis) {
+            $redis  = Redis::getConnection();
+            $key    = self::KEY_PREFIX . Config::get('app/app_name', '');
+            $number = $redis->incrby($key, $count);
+            for ($i = 0; $i < $count; $i++) {
+                yield intval(time() * 10000 + ($number - $count + $i + 1) * 10000 + rand(0, 10000));
+            }
+        } else {
+            for ($i = 0; $i < $count; $i++) {
+                yield intval(microtime(true) * 1000000) + mt_rand(1000000, 9999999);
+            }
+        }
+    }
+
+    /**
+     * @param int  $count    个数
+     * @param bool $useRedis 是否使用redis生成
      * @return array
      * @throws \Exception
      */
-    final public static function getList(int $count)
+    final public static function getList(int $count, bool $useRedis = true)
     {
-        $redis  = Redis::getConnection();
-        $key    = self::KEY_PREFIX . Config::get('app/app_name', '');
-        $number = $redis->incrby($key, $count);
         $idList = [];
-        for ($i = 0; $i < $count; $i++) {
-            $idList[] = intval(time() * 10000 + ($number - $count + $i + 1) * 10000 + rand(0, 10000));
+        if ($useRedis) {
+            $redis  = Redis::getConnection();
+            $key    = self::KEY_PREFIX . Config::get('app/app_name', '');
+            $number = $redis->incrby($key, $count);
+            for ($i = 0; $i < $count; $i++) {
+                $idList[] = intval(time() * 10000 + ($number - $count + $i + 1) * 10000 + rand(0, 10000));
+            }
+        } else {
+            for ($i = 0; $i < $count; $i++) {
+                $idList[] = intval(microtime(true) * 1000000) + mt_rand(1000000, 9999999);
+            }
         }
         return $idList;
     }
